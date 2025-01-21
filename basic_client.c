d3ef8856fbdc5d3a44665f5a2548fb3e1abede58
thasan50 <thasan3068@gmail.com>
2025-01-21 05:41:39 -0500

    Client, server files

--- basic_client.c
+++ basic_client.c
@@ -1,8 +1,28 @@
 #include "pipe_networking.h"
 
 int main() {
-  int to_server;
-  int from_server;
+    int to_server;
+    int from_server;
 
-  from_server = client_handshake( &to_server );
+    printf("Client: Starting handshake...\n");
+    from_server = client_handshake(&to_server);
+
+    // Test the handshake by sending and receiving a byte
+    char test_byte = 'C';  // Example byte from client
+    if (write(to_server, &test_byte, sizeof(test_byte)) == -1) {
+        perror("Client: Failed to write to server");
+        exit(1);
+    }
+    printf("Client: Sent test byte '%c' to server\n", test_byte);
+
+    char response_byte;
+    if (read(from_server, &response_byte, sizeof(response_byte)) == -1) {
+        perror("Client: Failed to read from server");
+        exit(1);
+    }
+    printf("Client: Received test byte '%c' from server\n", response_byte);
+
+    printf("Client: Handshake and test completed successfully\n");
+
+    return 0;
 }
--- basic_server.c
+++ basic_server.c
@@ -1,9 +1,29 @@
 #include "pipe_networking.h"
 
 int main() {
-  int to_client;
-  int from_client;
-  server_setup();
-  from_client = server_handshake( &to_client );
-  
+    int to_client;
+    int from_client;
+
+    printf("Server: Waiting for client handshake...\n");
+    from_client = server_handshake(&to_client);
+
+    // Test the handshake by receiving and responding with a byte
+    char received_byte;
+    if (read(from_client, &received_byte, sizeof(received_byte)) == -1) {
+        perror("Server: Failed to read from client");
+        exit(1);
+    }
+    printf("Server: Received test byte '%c' from client\n", received_byte);
+
+    char response_byte = 'S';  // Example byte from server
+    if (write(to_client, &response_byte, sizeof(response_byte)) == -1) {
+        perror("Server: Failed to write to client");
+        exit(1);
+    }
+    printf("Server: Sent test byte '%c' to client\n", response_byte);
+
+    printf("Server: Handshake and test completed successfully\n");
+
+    return 0;
 }
+
